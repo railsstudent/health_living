4ab2db035cd80517daef4091516f66d4fa473f4a
Connie Leung <cleung334@gmail.com>
2015-08-23 19:00:52 +0800

    Add modal to add new user.
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- app/scripts/controllers/subscribers.js
+++ app/scripts/controllers/subscribers.js
@@ -8,7 +8,7 @@
  * Controller of the hlApp
  */
 angular.module('hlApp')
-  .controller('SubscribersCtrl', function ($scope, $http) {
+  .controller('SubscribersCtrl', function ($scope, $http, $modal) {
  	$http.get('subscribers.json').then(function (response) {
  		$scope.subscribers = response.data;
  	});
@@ -30,6 +30,40 @@ angular.module('hlApp')
  	};
 
  	$scope.showModal = function _showModal() {
+    $scope.newUser = {};
+    var modalInstance = $modal.open({
+      templateUrl: 'views/add-user.html',
+      controller: 'AddNewUserCtrl',
+      resolve : {
+        newUser : function() {
+          return $scope.newUser;
+        }
+      }
+    });
 
+    modalInstance.result.then(function (selectedItem) {
+      $scope.subscribers.push({
+        no : $scope.subscribers.length + 1,
+        name: selectedItem.name,
+        userType: selectedItem.userType,
+        loyalty : selectedItem.loyalty,
+        joinDate: selectedItem.joinDate
+      });
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
  	};
-  });
+})
+ .controller('AddNewUserCtrl', function ($scope, $modalInstance, newUser) {
+
+    $scope.cancel = function _cancel() {
+      $modalInstance.dismiss('cancel');
+    };
+
+    $scope.newUser = newUser;
+
+    $scope.saveNewUser = function _saveNewUser() {
+      $modalInstance.close(newUser);
+    };
+
+ });
